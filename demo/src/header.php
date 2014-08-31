41e5e8274543a49ed2480bd1db469a66972c7bc9
Fabio <fabio300@gmail.com>
2014-08-31 03:24:46 -0300

    header url change

--- demo/src/header.php
+++ demo/src/header.php
@@ -10,7 +10,7 @@
                 <a href="/" class="brand">&nbsp;</a>
                 <div class="nav-collapse collapse top-nav">
                     <div id="langselect">
-                        <a href="/about?locale=pt" title="Português">Português</a>/<a href="/page/about?locale=en" title="English">English</a>
+                        <a href="about?locale=pt" title="Português">Português</a>/<a href="/page/about?locale=en" title="English">English</a>
                     </div>
                     <ul class="nav nav-pills">
 
